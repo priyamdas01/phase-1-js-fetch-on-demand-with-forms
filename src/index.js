2d162045392e953e713d70bf755b9e20b3883c41
Priyam Das <domidas01@gmail.com>
2023-05-31 03:15:31 -0400

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 const init = () => {
-  
+    const inputForm = document.querySelector("form");
+    inputForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+        const input = document.querySelector("input#searchByID");
+      
+        console.log(input.value);
+
+        fetch(`http://localhost:3000/movies/${input.value}`)
+        .then(res=>res.json())
+        .then(data=>{
+            document.querySelector("section#movieDetails h4").innerText = data.title;
+            document.querySelector("section#movieDetails p").innerText = data.summary;
+        });
+  })
 }
 
 document.addEventListener('DOMContentLoaded', init);
